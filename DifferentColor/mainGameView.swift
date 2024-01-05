42b6f33add0cc52b3d42d3cb465ee41cc8cba8c8
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 00:12:08 -0500

    Basic Setup

---
+++ DifferentColor/mainGameView.swift
@@ -0,0 +1,18 @@
+//
+//  mainGameView.swift
+//  DifferentColor
+//
+//  Created by Aryan Jain on 1/5/24.
+//
+
+import SwiftUI
+
+struct mainGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mainGameView()
+}
