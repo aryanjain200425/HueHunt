1803ce744b04fdc1b6e0ee8d63340941969ec87d
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 00:41:07 -0500

    UI For score

--- DifferentColor/mainGameView.swift
+++ DifferentColor/mainGameView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct mainGameView: View {
+    var score: Int = 0
     var body: some View {
+        VStack{
+            VStack{
+                Text("Score")
+                Text(String(score))
+            }.padding(.bottom, 50.0).font(.largeTitle).foregroundColor(Color.black)
+        }
         TileButton(color: Color.green, chosenOne: false)
     }
 }
