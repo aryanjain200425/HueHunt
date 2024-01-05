9ce0f3bc9dd511334d9d9074567b52d13865c896
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 00:37:49 -0500

    Displaying tile

--- DifferentColor/mainGameView.swift
+++ DifferentColor/mainGameView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct mainGameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TileButton(color: Color.green, chosenOne: false)
     }
 }
 
