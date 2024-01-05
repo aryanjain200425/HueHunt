720f4465152d00dc391459d93121c9776c85bdc6
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 00:08:26 -0500

    Initial Commit

---
+++ DifferentColor/DifferentColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DifferentColorApp.swift
+//  DifferentColor
+//
+//  Created by Aryan Jain on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DifferentColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
