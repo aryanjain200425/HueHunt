706a5aeb0e742a6796527949d33a84c8a25835d7
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 20:53:09 -0500

    Manual Board

--- DifferentColor/mainGameView.swift
+++ DifferentColor/mainGameView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct mainGameView: View {
     var score: Int = 0
+    var highScore: Int = 0
     var body: some View {
         VStack{
+                //MARK: Score
+                VStack{
+                    Text("Score")
+                    Text(String(score))
+                }
+                .padding(.bottom, 50)
+                .font(.largeTitle)
+                .foregroundColor(Color.black)
+            
+            //MARK: Board
             VStack{
-                Text("Score")
-                Text(String(score))
-            }.padding(.bottom, 50.0).font(.largeTitle).foregroundColor(Color.black)
+                HStack{
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                }
+                HStack{
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                }
+                HStack{
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                    TileButton(color: Color.green, chosenOne: false)
+                }
+                
+            }
         }
-        TileButton(color: Color.green, chosenOne: false)
     }
 }
 
