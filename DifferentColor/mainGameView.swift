594912c6f4d0f6427dd7d695e02a8841d784db8e
aryanjain200425 <aryanrashijain@gmail.com>
2024-01-05 21:03:38 -0500

    code to automate the board

--- DifferentColor/mainGameView.swift
+++ DifferentColor/mainGameView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct mainGameView: View {
-    var score: Int = 0
-    var highScore: Int = 0
+    @State var score: Int = 0
+    
+    
+    @State var board: [[TileButton]] = {
+        var counter = 0
+        
+        var rand = Int.random(in: 0...8)
+        
+        var views = [[TileButton]](repeating: [TileButton](repeating: TileButton(color: Color.green, chosenOne: false), count: 3), count: 3)
+        
+        var newColor = Color.red
+        
+        var maybeChosen = false
+        
+        for i in 0..<3 {
+            for j in 0..<3 {
+                if (counter == rand){
+                    maybeChosen = true
+                    newColor = Color.blue
+                }
+                else{
+                    maybeChosen = false
+                    newColor = Color.green
+                }
+                
+                views[i][j] = TileButton(color: newColor, chosenOne: maybeChosen)
+                
+                counter = counter + 1
+                
+            }
+        }
+        return views
+    }()
+    
+    
+    
     var body: some View {
         VStack{
                 //MARK: Score
